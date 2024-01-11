7387b20d952563786b808a85b66a1dc6644f298f
giorgia-nadizar <giorgia.nadizar@gmail.com>
2024-01-11 00:14:08 -0500

    MarioGPT for level generation

---
+++ mario_level_generator.py
@@ -0,0 +1,48 @@
+from mario_gpt import MarioLM
+
+quantifiers = ["no", "little", "some", "many"]
+pipes = [f"{n} pipes" for n in quantifiers]
+enemies = [f"{n} enemies" for n in quantifiers]
+blocks = [f"{n} blocks" for n in quantifiers]
+elevations = ["", "low elevation", "high elevation"]
+
+all_prompts = []
+for pipe in pipes:
+    for enemy in enemies:
+        for block in blocks:
+            for elevation in elevations:
+                prompt = f"{pipe}, {enemy}, {block}"
+                if len(elevation) > 0:
+                    prompt += f", {elevation}"
+                all_prompts.append(prompt)
+
+mario_lm = MarioLM()
+
+for i, prompt in enumerate(all_prompts):
+    print(f"{i}/{len(all_prompts)} - {prompt}")
+    file_name = f"levels/{prompt.replace(' ', '_')}"
+    # generate level of size 1400, pump temperature up to ~2.4 for more stochastic but playable levels
+    generated_level = mario_lm.sample(
+        prompts=[prompt],
+        num_steps=1400,
+        temperature=2.0,
+        use_tqdm=True
+    )
+
+    # show string list
+    print(generated_level.level)
+
+    # save image
+    generated_level.img.save(file_name + ".png")
+
+    # save text level to file
+    generated_level.save(file_name + ".txt")
+
+    # Continue generation
+    # generated_level_continued = mario_lm.sample(
+    #     seed=generated_level,
+    #     prompts=prompts,
+    #     num_steps=1400,
+    #     temperature=2.0,
+    #     use_tqdm=True
+    # )
